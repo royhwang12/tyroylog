1bbc7134b452ddf2c1ab322fe3a5a298ae5041c2
royhwang12 <royhwang22@gmail.com>
2022-07-10 17:42:44 -0700

    redeploy script edited

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-
-pip install -r requirements.txt
-
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
