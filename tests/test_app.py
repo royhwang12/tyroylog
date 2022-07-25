9499dac58e76f12e8fd78dbc3d7173fc148c14cc
royhwang12 <royhwang22@gmail.com>
2022-07-24 17:22:39 -0700

    timeline site changed

--- tests/test_app.py
+++ tests/test_app.py
@@ -63,7 +63,7 @@ class AppTestCase(unittest.TestCase):
 
         # Make a POST request
         response = self.client.post("/api/timeline_post", data={
-            "name": name,
+            "name": Roy Hwang,
             "email": email,
             "content": content
         })
